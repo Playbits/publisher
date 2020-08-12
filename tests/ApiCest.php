02803748b4890a93323b9da01cfbe75d3247cbd2
Abiola Adele <abiola.adele@andela.com>
2020-08-12 14:49:01 +0100

    update test values

--- tests/ApiCest.php
+++ tests/ApiCest.php
@@ -1,22 +1,24 @@
 <?php
-class ApiCest 
-{    
-    public function tryApi(ApiTester $I)
-    {
+class ApiCest {
+    public function tryApi(ApiTester $I) {
         $I->sendGET('/');
         $I->seeResponseCodeIs(404);
         $I->seeResponseIsJson();
     }
-    public function trySubscription(ApiTester $I)
-    {
-        $I->sendPOST('/subscribe/topic1');
+    public function trySubscription(ApiTester $I) {
+        $I->sendPOST('/subscribe/topic1', [
+            "url" => "http://localhost:8000/event",
+        ]);
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
     }
-    public function trPublish(ApiTester $I)
-    {
-        $I->sendPOST('/publish/topic1');
+    public function trPublish(ApiTester $I) {
+        $I->sendPOST('/publish/topic1', [
+            "message" => "This message will be broadcasted to all the urls in topic file",
+        ]);
         $I->seeResponseCodeIs(200);
+        $I->haveHttpHeader('accept', 'application/json');
+        $I->haveHttpHeader('content-type', 'application/json');
         $I->seeResponseIsJson();
     }
 }
